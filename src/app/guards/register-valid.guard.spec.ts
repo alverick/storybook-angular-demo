57660dd214922976925a6ee9c2438295e0eb66c5
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-01-04 16:07:24 -0500

    fix: update test

--- src/app/guards/register-valid.guard.spec.ts
+++ src/app/guards/register-valid.guard.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { RegisterValidGuard } from './register-valid.guard';
@@ -6,7 +7,7 @@ describe('RegisterValidGuard', () => {
   let guard: RegisterValidGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
     guard = TestBed.inject(RegisterValidGuard);
   });
 
